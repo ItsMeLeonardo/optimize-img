97c23eadf78c010ac4b5e64da9eb20850455c4d3
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-26 18:42:16 -0500

    add slint and prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#a855f7',
+        secondary: '#f7a855',
+        tertiary: '#ec4899',
+        text_color: '#1f2937',
+      },
+    },
+    fontFamily: {
+      sans: ['DM Sans', 'system-ui', 'sans-serif'],
+    },
   },
   plugins: [],
-};
+}
