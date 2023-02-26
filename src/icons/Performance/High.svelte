f709f709077cbff2513b6ed805989c74ed318b43
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-26 18:42:50 -0500

    add icons

---
+++ src/icons/Performance/High.svelte
@@ -0,0 +1,23 @@
+<svg
+  width="24px"
+  height="24px"
+  stroke-width="1.5"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  color="currentColor"
+>
+  <path
+    d="M7 12.5l3 3 7-7"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  /><path
+    d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
