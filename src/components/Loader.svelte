67c20a5c9751c7318d241e765546bfec0869074f
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-26 18:43:42 -0500

    add loader

---
+++ src/components/Loader.svelte
@@ -0,0 +1,38 @@
+<style>
+  .loader {
+    --size: 34px;
+    width: var(--size);
+    height: var(--size);
+    display: inline-block;
+    position: relative;
+  }
+  .loader::after,
+  .loader::before {
+    content: '';
+    box-sizing: border-box;
+    width: var(--size);
+    height: var(--size);
+    border-radius: 50%;
+    border: 2px solid rgb(100, 116, 139);
+    position: absolute;
+    left: 0;
+    top: 0;
+    animation: animloader 2s linear infinite;
+  }
+  .loader::after {
+    animation-delay: 1s;
+  }
+
+  @keyframes animloader {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 0;
+    }
+  }
+</style>
+
+<span class="loader" />
