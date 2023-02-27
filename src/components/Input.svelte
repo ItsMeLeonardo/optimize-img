f88ae1555720d3204155c50db1a6902ea8064ae0
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-27 18:47:33 -0500

    add number range validation

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -7,11 +7,30 @@
     | 'search'
     | 'tel'
     | 'url'
-  export const type: HTMLInputTypeAttribute = 'text'
+  export let type: HTMLInputTypeAttribute = 'text'
   export let value: string | number = ''
+  export let min: number | string = ''
+  export let max: number | string = ''
 
   const handleInput = (e: Event) => {
     const target = e.target as HTMLInputElement
+
+    if (type === 'number' && min && max) {
+      const value = Number(target.value)
+
+      if (value < min) {
+        target.value = String(min)
+        return
+      }
+
+      if (value > max) {
+        target.value = String(max)
+        return
+      }
+
+      return
+    }
+
     value = target.value
   }
 </script>
@@ -20,8 +39,10 @@
   <input
     {value}
     {type}
+    {min}
+    {max}
     on:input={handleInput}
-    class="text-xs p-2 rounded-lg bg-white w-full"
+    class="text-xs p-2 rounded-lg bg-white w-full appearance-none"
   />
 
   <span class="flex items-center justify-center w-[18px] aspect-square">
